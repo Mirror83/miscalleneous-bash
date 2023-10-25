f0d7caffe1a479b1b2fa77d8b928c9ae5db2e6ae
Mirror83 <glenochieng045@gmail.com>
2023-10-25 06:14:19 +0300

    Implemented the functionality as per question 4

--- read-empty.sh
+++ read-empty.sh
@@ -1,2 +1,9 @@
-read -p "Enter path to directory: " line
-echo $line
+# Find a way to do it in only one call to find
+read -r -p "Enter path to directory: " line
+
+printf "\n%-50s %-50s\n" "Empty directory" "Time created"
+find "$line" -type d -empty -exec stat -c "%-50n %-50w" {} \;
+
+echo ""
+empty_dir_count=$(find "$line" -type d -empty | wc -l)
+echo "Empty directory count: $empty_dir_count"
