65e9ec46ae7a413478f019219a337d0bcbcde43d
Mirror83 <glenochieng045@gmail.com>
2023-10-25 07:11:50 +0300

    Added shebang and opted for table output

--- logtime.sh
+++ logtime.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Get the UID from the user
-read -p "Enter the user's UID: " uid
+#! /bin/bash
 
-# Count the number of times the user is logged in
-# The `who` command will get the currently logged in users
-# `grep "^$uid"` will get the lines in which the user with the
-# provided uid is logged in
-# `wc -l` will return the count of these lines
-login_count=$(who -a | grep "^$uid" | wc -l)
+read -r -p "Enter the user's UID: " uid
+user_name=$(id -u --name "$uid")
+
+# Calculate login count
+login_time=$(last | grep -c "^$user_name")
 
 # Display the login count
-echo "The user with UID $uid is logged in $login_count time(s)."
+printf "\n%-10s %-20s %-10s\n" "UID" "User name" "Login count"
+printf "%-10s %-20s %-10s\n" "$uid" "$user_name" "$login_time"
+
