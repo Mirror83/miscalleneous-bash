bb56c4d0dcd0904d832f197c861bb31848450701
Mirror83 <glenochieng045@gmail.com>
2023-10-25 06:07:31 +0300

    Added minor syntax-related improvements

--- word-count.sh
+++ word-count.sh
@@ -1,10 +1,10 @@
-words=`cat "JUJAyetu.txt"`
+words=$(cat "JUJAyetu.txt")
 
 words_total=${#words}
 word_count=0
 i=0
 
-while [ $i -lt $words_total ]; do
+while [ $i -lt "$words_total" ]; do
     # The syntax ${string:offset:num} returns a substring of num characters from
     # position offset in the string
     # Here, it is leveraged to obtain each character in the string
