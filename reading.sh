c2f2171a2fe83bc2b819fa48460c3568270e8ffe
Mirror83 <glenochieng045@gmail.com>
2023-10-25 06:06:17 +0300

    Replaced depracated $[] with $(())

--- reading.sh
+++ reading.sh
@@ -6,9 +6,9 @@ iterations=0
 echo "Sum 5"
 while [ $iterations -lt 5 ];
 do
-    read -p "Enter value $[iterations+1]: " num
-    sum=$[sum + num]
-    iterations=$[iterations + 1]
+    read -r -p "Enter value $((iterations + 1)): " num
+    sum=$((sum + num))
+    iterations=$((iterations + 1))
 done
 
 echo "Sum: $sum"
