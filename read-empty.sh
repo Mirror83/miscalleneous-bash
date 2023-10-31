5f67c76933847dd73278a890603faf20f95ee7a0
Mirror83 <glenochieng045@gmail.com>
2023-10-31 21:18:51 +0300

    Added comment on the first find command

--- read-empty.sh
+++ read-empty.sh
@@ -3,6 +3,11 @@
 read -r -p "Enter path to directory: " line
 
 printf "\n%-50s %-50s\n" "Empty directory" "Time created"
+
+# The command passed in for the exec option executes the command
+# (in this case stat) foreach match it, well, finds.
+# The stat command gives the name and time of file birth for the file
+# (due to the %n and %w in its -c(format) argument)
 find "$line" -type d -empty -exec stat -c "%-50n %-50w" {} \;
 
 echo ""
